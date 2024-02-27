780bb93f8c6455d10df834b641d97e8ed8daa691
MariuszMalankiewicz <m2ario123@wp.pl>
2024-02-27 12:09:00 +0100

    Update about component
    adding template contains responsivness from mobile to desktop

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
 import Nav from './components/Nav.vue'
+import About from './components/About.vue'
+import Portfolio from './components/Portfolio.vue'
 </script>
 
 <template>
     <main>
         <Nav></Nav>
+        <About></About>
+        <Portfolio></Portfolio>
+        
     </main>
 </template>
 
