07c3b8fe61595dd684b566b4c5f8734b2c0fd874
MariuszMalankiewicz <m2ario123@wp.pl>
2024-02-28 11:33:11 +0100

    Removing unnecessary tags
    removing tags script, style in portfolio and about components

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,9 @@
-<script setup>
-</script>
-
 <template>
-    <section class="min-w-[320px] px-5 pt-16 mb-16">
-        <h1 id="about" class="text-[28px] sm:text-[32px] xl:text-[36px] 2xl:text-[40px] text-center font-bold font-Montserrat">About</h1>
+    <section class="min-w-[320px] px-5 pt-16 mb-16 font-Montserrat">
+        <h1 id="about" class="text-[28px] sm:text-[32px] xl:text-[36px] 2xl:text-[40px] text-center font-bold">About</h1>
         <div class="flex flex-col md:flex-row md:justify-center items-center md:space-x-16 lg:space-x-32 2xl:space-x-64 mt-16">
             <img src="../assets/images/Work in progress-amico.png" class="max-w-[250px] sm:max-w-[320px] md:max-w-[300px] lg:max-w-[340px] xl:max-w-[380px] 2xl:max-w-[420px]" alt="work in progress">
-            <p class="max-w-[300px] sm:max-w-[320px] md:max-w-[300px] lg:max-w-[340px] xl:max-w-[380px] 2xl:max-w-[420px] text-[28px] sm:text-[32px] xl:text-[36px] 2xl:text-[40px] font-semibold pt-16 md:pt-0 font-Montserrat">Hi I’am Mariusz Malankiewicz i have experience with web design and website.</p>
+            <p class="max-w-[300px] sm:max-w-[320px] md:max-w-[300px] lg:max-w-[340px] xl:max-w-[380px] 2xl:max-w-[420px] text-[28px] sm:text-[32px] xl:text-[36px] 2xl:text-[40px] font-semibold pt-16 md:pt-0">Hi I’am Mariusz Malankiewicz i have experience with web design and website.</p>
         </div>
     </section>
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="min-w-[320px] font-Montserrat">
+        <img src="../assets/images/wave_up.svg" alt="wave up">
+        <div class="bg-blue text-white pb-6">
+            <h1 id="portfolio" class="text-[28px] sm:text-[32px] xl:text-[36px] 2xl:text-[40px] text-center font-bold pt-8">Portfolio</h1>
+            <nav class="flex justify-center items-center space-x-4 py-8">
+                <a href="#" class="hover:underline hover:font-semibold transition-all duration-150 ease-in-out">All</a>
+                <a href="#" class="hover:underline hover:font-semibold transition-all duration-150 ease-in-out">Web design</a>
+                <a href="#" class="hover:underline hover:font-semibold transition-all duration-150 ease-in-out">Website</a>
+            </nav>
+            <div class="bg-white w-[320px] rounded-xl text-black shadow-xl mx-auto mb-6">
+                <img src="../assets/images/portfolio.png" alt="portfolio project" class="rounded-xl">
+                <h1 class="font-bold text-xl ps-2 pt-4">Portfolio</h1>
+                <div class="w-full flex flex-wrap pt-3 text-sm">
+                    <p class="mx-2 mt-2 px-1 py-[1px] bg-dark-gray rounded-md">Design</p>
+                    <p class="me-2 mt-2 px-1 py-[1px] bg-dark-gray rounded-md">Website</p>
+                    <p class="me-2 mt-2 px-1 py-[1px] bg-light-gray rounded-md">Vue</p>
+                    <p class="mt-2 px-1 py-[1px] bg-light-gray rounded-md">Tailwind</p>
+                </div>
+                <hr class="w-full h-1 text-light-gray mt-6">
+                <div class="flex flex-nowrap items-center pt-3 pb-6">
+                    <a href="#" class="mx-2 mt-2 px-2 py-1 bg-blue hover:bg-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Website</a>
+                    <a href="#" class="mx-2 mt-2 px-2 py-1 bg-light-blue hover:bg-light-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Web design</a>
+                    <a href="#" class="mx-2 mt-2 px-2 py-1 bg-dark-gray hover:bg-dark-gray/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Github</a>
+                </div>
+
+            </div>
+        </div>
+        <img src="../assets/images/wave_down.svg" alt="wave down">
+    </section>
+</template>
