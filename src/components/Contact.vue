36160c943a9c10d0ffb464361fb47a9121385173
MariuszMalankiewicz <m2ario123@wp.pl>
2024-03-01 11:59:11 +0100

    Modified contact component
    add email input, change message input to textarea

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,8 +4,9 @@
         <div class="w-full flex flex-col justify-center items-center md:flex-row md:space-x-10 lg:space-x-20 xl:space-x-28 2xl:space-x-36">
             <img src="../assets/images/Contact us-rafiki.png" class="max-w-[250px] sm:max-w-[320px] md:max-w-[300px] lg:max-w-[340px] xl:max-w-[380px] 2xl:max-w-[420px]" alt="Contact us-rafiki.png">
             <form action="#" class="w-3/4 md:w-[300px] lg:w-[340px] xl:w-[380px] 2xl:w-[420px]">
-            <label for="name" class="flex flex-col mt-4"><p class="text-dark-gray">Name</p><input type="text" class="border-b-2 border-dark-gray focus:outline-none"></label>
-            <label for="message" class="flex flex-col mt-4"><p class="text-dark-gray">Message</p><input type="text" class="border-b-2 border-dark-gray focus:outline-none"></label>
+            <label for="name" class="flex flex-col mt-4"><p class="text-dark-gray">Name</p><input type="text" placeholder="Your name" class="placeholder:text-xs placeholder:ps-1 placeholder:pt-1 focus:text-dark-gray focus:text-text-sm text-dark-gray border-b-2 border-dark-gray focus:outline-none"></label>
+            <label for="email" class="flex flex-col mt-4"><p class="text-dark-gray">Email</p><input type="email" placeholder="Your email" class="placeholder:text-xs placeholder:ps-1 placeholder:pt-1 focus:text-dark-gray focus:text-text-sm text-dark-gray border-b-2 border-dark-gray focus:outline-none"></label>
+            <label for="message" class="flex flex-col mt-4"><p class="text-dark-gray">Message</p><textarea placeholder="Your message" class="placeholder:text-xs placeholder:ps-2 placeholder:pt-1 focus:text-dark-gray focus:text-text-sm text-dark-gray focus:ps-1 border-b-2 border-l-2 border-r-2 rounded-b-md border-dark-gray focus:outline-none" cols="30" rows="3"></textarea></label>
             <input type="submit" value="Send" class="bg-blue text-white mt-4 rounded-md px-4 sm:px-6 py-2 hover:cursor-pointer hover:font-semibold hover:bg-blue/90 transition-all duration-150 ease-in-out">
             </form>
         </div>
