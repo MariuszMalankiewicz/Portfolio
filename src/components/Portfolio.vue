6699e9206c5b171ca5d817895d680728bc4d12aa
MariuszMalankiewicz <m2ario123@wp.pl>
2024-03-11 12:21:28 +0100

    Update Portfolio component
    Add swiper library to portfolio component import (swiper, swiperSlide,
    navigation) and style (swiper, navigation), add swiperslide mmblog and
    portfolio, add style for swiper button prev and next

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,31 +1,75 @@
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation } from "swiper/modules";
+
+import 'swiper/css/navigation';
+import 'swiper/css';
+
+</script>
 <template>
     <section id="projects" class="min-w-[320px] font-Montserrat">
         <img src="../assets/images/wave_up.svg" alt="wave up">
         <div class="bg-blue text-white pb-6">
-            <h1 class="text-[28px] sm:text-[32px] xl:text-[36px] 2xl:text-[40px] text-center font-bold pt-8">Projects</h1>
-            <nav class="flex justify-center items-center space-x-4 py-8">
-                <a href="#" class="hover:underline hover:font-semibold transition-all duration-150 ease-in-out">All</a>
-                <a href="#" class="hover:underline hover:font-semibold transition-all duration-150 ease-in-out">Web design</a>
-                <a href="#" class="hover:underline hover:font-semibold transition-all duration-150 ease-in-out">Website</a>
-            </nav>
-            <div class="bg-white w-[320px] rounded-xl text-black shadow-xl mx-auto mb-6">
-                <img src="../assets/images/portfolio.png" alt="portfolio project" class="rounded-xl">
-                <h1 class="font-bold text-xl ps-2 pt-4">Portfolio</h1>
-                <div class="w-full flex flex-wrap pt-3 text-sm">
-                    <p class="mx-2 mt-2 px-1 py-[1px] bg-dark-gray rounded-md">Website</p>
-                    <p class="me-2 mt-2 px-1 py-[1px] bg-dark-gray rounded-md">Web design</p>
-                    <p class="me-2 mt-2 px-1 py-[1px] bg-light-gray rounded-md">Vue</p>
+            <h1 class="text-[28px] sm:text-[32px] xl:text-[36px] 2xl:text-[40px] text-center font-bold py-8">Projects</h1>
+            <Swiper :modules="[Navigation]" navigation class="md:w-2/3 lg:w-1/2"> 
+              <SwiperSlide>
+                <div class="bg-white w-[320px] rounded-xl text-black shadow-xl mx-auto mb-6">
+                  <img src="/src/assets/images/mmblog.png" alt="portfolio images" class="rounded-t-xl">
+                  <h1 class="font-bold text-xl ps-2 pt-4">MMblog</h1>
+                  <div class="w-full flex flex-wrap pt-3 text-sm">
+                    <p class="mx-2 mt-2 px-1 py-[1px] bg-light-gray rounded-md">Laravel</p>
                     <p class="mt-2 px-1 py-[1px] bg-light-gray rounded-md">Tailwind</p>
                 </div>
-                <hr class="w-full h-1 text-light-gray mt-6">
-                <div class="flex flex-nowrap items-center pt-3 pb-6">
-                    <a href="https://mariuszmalankiewicz.netlify.app/" target="blank" class="mx-2 mt-2 px-2 py-1 bg-blue hover:bg-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Website</a>
-                    <a href="https://dribbble.com/shots/23744671-Portfolio-web-design?utm_source=Clipboard_Shot&utm_campaign=mariusz_malankiewicz&utm_content=Portfolio%20web%20design&utm_medium=Social_Share" target="blank" class="mx-2 mt-2 px-2 py-1 bg-light-blue hover:bg-light-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Web design</a>
-                    <a href="https://github.com/MariuszMalankiewicz/Portfolio" target="blank" class="mx-2 mt-2 px-2 py-1 bg-dark-gray hover:bg-dark-gray/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Github</a>
+                  <hr class="w-full h-1 text-light-gray mt-6">
+                  <div class="flex flex-nowrap items-center pt-3 pb-6">
+                      <a href="http://mmblog.c1.is/public/" target="blank" class="mx-2 mt-2 px-2 py-1 bg-blue hover:bg-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Website</a>
+                      <a href="#" target="blank" class="mx-2 mt-2 px-2 py-1 bg-light-blue hover:bg-light-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Web design</a>
+                      <a href="https://github.com/MariuszMalankiewicz/MMBlog" target="blank" class="mx-2 mt-2 px-2 py-1 bg-dark-gray hover:bg-dark-gray/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Github</a>
+                  </div>
                 </div>
+              </SwiperSlide>
 
-            </div>
+              <SwiperSlide>
+                <div class="bg-white w-[320px] rounded-xl text-black shadow-xl mx-auto mb-6">
+                  <img src="/src/assets/images/portfolio.png" alt="portfolio images" class="rounded-t-xl">
+                  <h1 class="font-bold text-xl ps-2 pt-4">Portfolio</h1>
+                  <div class="w-full flex flex-wrap pt-3 text-sm">
+                    <p class="mx-2 mt-2 px-1 py-[1px] bg-light-gray rounded-md">Vue</p>
+                    <p class="mt-2 px-1 py-[1px] bg-light-gray rounded-md">Tailwind</p>
+                </div>
+                  <hr class="w-full h-1 text-light-gray mt-6">
+                  <div class="flex flex-nowrap items-center pt-3 pb-6">
+                      <a href="https://mariuszmalankiewicz.netlify.app/" target="blank" class="mx-2 mt-2 px-2 py-1 bg-blue hover:bg-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Website</a>
+                      <a href="#" target="blank" class="mx-2 mt-2 px-2 py-1 bg-light-blue hover:bg-light-blue/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Web design</a>
+                      <a href="https://github.com/MariuszMalankiewicz/Portfolio" target="blank" class="mx-2 mt-2 px-2 py-1 bg-dark-gray hover:bg-dark-gray/90 hover:font-semibold text-white rounded-md transition-all duration-150 ease-in-out">Github</a>
+                  </div>
+                </div>
+              </SwiperSlide>
+              
+            </Swiper>
         </div>
         <img src="../assets/images/wave_down.svg" alt="wave down">
     </section>
 </template>
+
+<style> 
+.swiper-button-next,
+.swiper-button-prev {
+    color: #ffffff !important;
+}
+
+@media (max-width:430px) {
+  .swiper-button-next,
+  .swiper-button-prev {
+    display:none
+}
+}
+@media (max-width:767px) {
+  .swiper-button-prev {
+    padding-left: 7%;
+}
+.swiper-button-next{
+  padding-right: 7%;
+}
+}
+</style>
