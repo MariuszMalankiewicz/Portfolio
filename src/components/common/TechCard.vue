d7d1a8b408fe2f0058d920282ba5043a183743ec
MariuszMalankiewicz <m2ario123@wp.pl>
2024-10-04 15:56:07 +0200

    update techstack icons rwd

---
+++ src/components/common/TechCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gray-100 p-[12px] sm:p-[16px] md:p-[20px] lg:p-[24px] text-[32px] sm:text-[36px] md:text-[40px] lg:text-[44px] rounded-2xl">
+        <slot></slot>
+    </div>
+</template>
--- src/components/pages/TechStackPage.vue
+++ src/components/pages/TechStackPage.vue
@@ -1,4 +1,6 @@
 <script setup>
+import TechCard from '../common/TechCard.vue';
+
 import { DeHtml5Original } from '@kalimahapps/vue-icons';
 import { DeCss3Original } from '@kalimahapps/vue-icons';
 import { DeJavascriptOriginal } from '@kalimahapps/vue-icons';
@@ -23,61 +25,24 @@ import { AnFilledFileMarkdown } from '@kalimahapps/vue-icons';
     <section class="px-4 mt-[80px] font-Montserrat mb-12">
         <h1 class="text-[32px] sm:text-[44px] md:text-[52px] text-center font-bold text-blue">Tech Stack</h1>
         <div class="w-full md:w-[80%] lg:w-[70%] xl:w-[60%] mx-auto flex flex-wrap justify-center items-center gap-12 mt-12 2xl:mt-24 text-[40px]">
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DeHtml5Original></DeHtml5Original>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DeCss3Original></DeCss3Original>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DeJavascriptOriginal></DeJavascriptOriginal>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DeBootstrapOriginalWordmark></DeBootstrapOriginalWordmark>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <BxTailwindCss class="text-blue"></BxTailwindCss>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DeVuejsOriginal></DeVuejsOriginal>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DePhpOriginal></DePhpOriginal>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <CoBrandLaravel class="text-red-500"></CoBrandLaravel>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DeMysqlOriginalWordmark></DeMysqlOriginalWordmark>
-            </div>
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <BsFiletypeSql class="text-sky-500"></BsFiletypeSql>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <DeSqliteOriginal></DeSqliteOriginal>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <CoBrandJson class="text-gray-700"></CoBrandJson>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <SiPostman class="text-orange-500"></SiPostman>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <BxVisualStudio class="text-blue"></BxVisualStudio>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <AiFigshare class="text-blue"></AiFigshare>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <VsFileTypePhpunit></VsFileTypePhpunit>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <BsWordpress></BsWordpress>
-            </div> 
-            <div class="bg-gray-100 p-6 rounded-2xl">
-                <AnFilledFileMarkdown></AnFilledFileMarkdown>
-            </div> 
-            
+            <TechCard><DeHtml5Original></DeHtml5Original></TechCard>
+            <TechCard><DeCss3Original></DeCss3Original></TechCard>
+            <TechCard><DeJavascriptOriginal></DeJavascriptOriginal></TechCard>
+            <TechCard><DeBootstrapOriginalWordmark></DeBootstrapOriginalWordmark></TechCard>
+            <TechCard><BxTailwindCss class="text-blue"></BxTailwindCss></TechCard>
+            <TechCard><DeVuejsOriginal></DeVuejsOriginal></TechCard>
+            <TechCard><DePhpOriginal></DePhpOriginal></TechCard>
+            <TechCard><CoBrandLaravel class="text-red-500"></CoBrandLaravel></TechCard>
+            <TechCard><DeMysqlOriginalWordmark></DeMysqlOriginalWordmark></TechCard>
+            <TechCard><BsFiletypeSql class="text-sky-500"></BsFiletypeSql></TechCard>
+            <TechCard><DeSqliteOriginal></DeSqliteOriginal></TechCard>
+            <TechCard><CoBrandJson class="text-gray-700"></CoBrandJson></TechCard>
+            <TechCard><SiPostman class="text-orange-500"></SiPostman></TechCard>
+            <TechCard><BxVisualStudio class="text-blue"></BxVisualStudio></TechCard>
+            <TechCard><AiFigshare class="text-blue"></AiFigshare></TechCard>
+            <TechCard><VsFileTypePhpunit></VsFileTypePhpunit></TechCard>
+            <TechCard><BsWordpress></BsWordpress></TechCard>
+            <TechCard><AnFilledFileMarkdown></AnFilledFileMarkdown></TechCard>
         </div>
     </section>
 </template>
