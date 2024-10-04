d7ffed8c2029be4062c3f405969c369cc08dad52
MariuszMalankiewicz <m2ario123@wp.pl>
2024-10-04 12:38:36 +0200

    add more protject to portfolio

--- src/components/common/ImageCard.vue
+++ src/components/common/ImageCard.vue
@@ -1,16 +1,3 @@
-<template>
-    <div class="border-[1.5px] max-w-[360px] sm:max-w-[420px] text-start">
-        <img :src="imageSrc" alt="Card Image">
-        <h3 class="px-4 pt-6 pb-2 font-bold text-[19px] sm:text-[24px]">{{ title }}</h3>
-        <p class="px-4 pb-2 leading-[26px] text-[13px] sm:text-[14px]">{{ technology }}</p>
-        <p class="px-4 text-[15px] sm:text-[16px]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos rerum tempora, nulla quae, eos reprehenderit, fuga nesciunt odio est error quisquam asperiores cumque iste. Maiores magnam iste sapiente <span class="text-blue underline">read more...</span></p>
-        <div class="mb-6 ml-4 space-y-4 felx flex-wrap"> 
-            <button class="bg-blue/90 text-white mt-4 rounded-md px-4 py-1.5 mr-4">Live</button>
-            <button class="border-[1.5px] border-gray-300 text-gray-500 rounded-md px-4 py-1.5">Github</button>
-        </div>
-    </div>
-</template>
-
 <script setup>
 const props = defineProps({
     imageSrc: {
@@ -21,9 +8,34 @@ const props = defineProps({
         type: String,
         required: true,
     },
+    description: {
+        type: String,
+        required: true,
+    },
     technology: {
+        type: String,
+        required: true,
+    },
+    live: {
+        type: String,
+        required: true,
+    },
+    github: {
         type: String,
         required: true,
     },
 });
 </script>
+
+<template>
+    <div class="border-[1.5px] max-w-[360px] sm:max-w-[420px] text-start">
+        <img :src="imageSrc" alt="Card Image">
+        <h3 class="px-4 pt-6 pb-2 font-bold text-[19px] sm:text-[24px]">{{ title }}</h3>
+        <p class="px-4 pb-2 leading-[26px] text-[13px] sm:text-[14px]">{{ technology }}</p>
+        <p class="px-4 text-[15px] sm:text-[16px]">{{ description }}</p>
+        <div class="mb-6 ml-4 space-y-4 felx flex-wrap mt-4">
+            <a :href="live" target="_blank" class="bg-blue/90 text-white mt-4 rounded-md px-4 py-2 mr-4">Live</a>
+            <a :href="github" target="_blank" class="border-[1.5px] border-gray-300 text-gray-500 rounded-md px-4 py-2">Github</a>
+        </div>
+    </div>
+</template>
--- src/components/pages/PortfolioPage.vue
+++ src/components/pages/PortfolioPage.vue
@@ -6,10 +6,95 @@ import NavImageCardButton from '/src/components/common/NavImageCardButton.vue'
 
 import MMBlog from '/src/assets/images/mmblog.png'
 import Portfolio from '/src/assets/images/portfolio.png'
+import TaskManagmentSystem from '/src/assets/images/task-managment-system.png'
+import CarsSpot from '/src/assets/images/cars-spot.png'
+import MemoryGame from '/src/assets/images/memory-game.png'
+import ToDoList from '/src/assets/images/todo_list.png'
+import RockScissorsAndPaper from '/src/assets/images/rock-scissors-paper.png'
+import TreeStructure from '/src/assets/images/tree-structure.png'
+import ToDoList2 from '/src/assets/images/todo-list-2.png'
 
 const cards = ref([
-  {imageSrc: MMBlog, title: "MMBlog", technology: "Laravel | PHP | MySQL | Tailwind | JavaScript",},
-  {imageSrc: Portfolio, title: "Portfolio", technology: "Vue | Tailwind",},
+  {
+    imageSrc: MMBlog,
+    title: "MMBlog",
+    description: "Blogs allow people to share knowledge, experiences, and advice on a variety of topics. They can solve the problem of lack of access to information, helping users find answers to questions and learn new things in a simple and accessible way.",
+    technology: "Laravel | PHP | MySQL | Tailwind | JavaScript",
+    live: "#",
+    github: "https://github.com/MariuszMalankiewicz/MMBlog",
+  },
+  {
+    imageSrc: Portfolio,
+    title: "Portfolio",
+    description: "A portfolio website solves the problem of effectively showcasing skills and experience, allowing individuals to stand out in the job market. It facilitates professional presentation of projects, helps build a personal brand, and ensures accessibility and interactivity, which can attract clients or employers.",
+    technology: "Vue | Tailwind",
+    live: "https://mariuszmalankiewicz.netlify.app/",
+    github: "https://github.com/MariuszMalankiewicz/Portfolio",
+  },
+
+  {
+    imageSrc: TaskManagmentSystem,
+    title: "Task Managment System",
+    description: "A Task Management System solves the problem of organizing, prioritizing, and tracking task progress in projects. It facilitates task assignment, deadline monitoring, and team collaboration, which enhances work efficiency and eliminates chaos.",
+    technology: "Laravel | Postman",
+    live: "#",
+    github: "https://github.com/MariuszMalankiewicz/task-managment-system",
+  },
+
+  {
+    imageSrc: CarsSpot,
+    title: "Cars Spot",
+    description: "Cars Spot is a company dealing in car sales that has faced issues related to inadequate customer service, complaints handling, and poor organization of logistical processes. This led to delivery delays and customer dissatisfaction.",
+    technology: "Vue | Tailwind",
+    live: "https://cars-spott.netlify.app/",
+    github: "https://github.com/MariuszMalankiewicz/cars-spot",
+  },
+
+  {
+    imageSrc: MemoryGame,
+    title: "Memory Game",
+    description: "The Memory Game improves memory and concentration by exercising the ability to remember and recognize pairs of cards or images that are face down. It helps enhance cognitive skills, especially in children, and serves as memory training for adults.",
+    technology: "HTML5 | CSS3 | JavaScript",
+    live: "https://mariuszmalankiewicz.github.io/Memory-Game/",
+    github: "https://github.com/MariuszMalankiewicz/Memory-Game",
+  },
+
+  {
+    imageSrc: ToDoList,
+    title: "ToDo List",
+    description: "A to-do list solves the problem of task organization by helping with time management, prioritization, and remembering obligations. It facilitates planning and increases productivity.",
+    technology: "HTML5 | CSS3 | JavaScript",
+    live: "https://mariuszmalankiewicz.github.io/To-Do-List/",
+    github: "https://github.com/MariuszMalankiewicz/To-Do-List",
+  },
+
+  {
+    imageSrc: RockScissorsAndPaper,
+    title: "Rock Scissors And Paper",
+    description: "The Rock, Scissors, and Paper (RSP) problem involves simulating a game where two players simultaneously choose one of three options: rock, scissors, or paper. The rules are as follows Rock beats scissors. Scissors beat paper. Paper beats rock. The problem is to develop an algorithm that correctly determines the outcome of the game by comparing the choices of both players.",
+    technology: "HTML5 | CSS3 | JavaScript",
+    live: "https://mariuszmalankiewicz.github.io/Rock-scissors-paper/",
+    github: "https://github.com/MariuszMalankiewicz/Rock-scissors-paper",
+  },
+
+  {
+    imageSrc: TreeStructure,
+    title: "Tree Structure",
+    description: "A tree structure solves the problem of organizing and quickly searching hierarchical data, such as parent-child relationships. It enables efficient operations like searching, inserting, and deleting elements in complex data sets.",
+    technology: "Laravel | Bootstrap | JavaScript",
+    live: "#",
+    github: "https://github.com/MariuszMalankiewicz/Tree-Structure",
+  },
+
+  {
+    imageSrc: ToDoList2,
+    title: "ToDoList",
+    description: "A to-do list solves the problem of task organization by helping with time management, prioritization, and remembering obligations. It facilitates planning and increases productivity.",
+    technology: "Vue3 | Tailwind",
+    live: "https://vue-todo-list-mm.netlify.app/",
+    github: "https://github.com/MariuszMalankiewicz/Vue3TodoList",
+  },
+
 ]);
 </script>
 
@@ -23,7 +108,15 @@ const cards = ref([
         <NavImageCardButton class="cursor-no-drop line-through">Full-Stack</NavImageCardButton>
       </nav>
       <div class="mt-12 flex flex-wrap items-center justify-center gap-12">
-      <ImageCard v-for="(card, index) in cards" :key="index" :imageSrc="card.imageSrc" :title="card.title" :technology="card.technology"></ImageCard>
+      <ImageCard v-for="(card, index) in cards" 
+        :key="index" 
+        :imageSrc="card.imageSrc" 
+        :title="card.title" 
+        :description="card.description" 
+        :technology="card.technology"
+        :live="card.live"
+        :github="card.github">
+      </ImageCard>
     </div>
   </section>
   </template>
